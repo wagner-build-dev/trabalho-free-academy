ef51671a7bc70b3d82ab9b19207ce48eaad06e68
wagner-build-dev <wagner.build.dev@gmail.com>
2026-06-10 12:29:31 -0300

    Update conexao.php

--- config/conexao.php
+++ config/conexao.php
@@ -1,1 +1,13 @@
+<?php
+$host = "localhost";
+$usuario = "root";
+$senha = "";
+$banco = "comercio_eletronico";
 
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$banco;charset=utf8mb4", $usuario, $senha);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Erro na conexão: " . $e->getMessage());
+}
+?>
